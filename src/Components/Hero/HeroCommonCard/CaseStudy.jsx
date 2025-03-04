0ff171892d2446349a766f2d444550d43b29863b
Samithsr <samithrgowda@gmail.com>
2025-03-04 09:52:19 +0530

    use params

--- src/Components/Hero/HeroCommonCard/CaseStudy.jsx
+++ src/Components/Hero/HeroCommonCard/CaseStudy.jsx
@@ -1,8 +1,50 @@
 import React from 'react'
+import './CaseStudy.css'
+import visual from "../HeroCardImages/visualsearch.jpg"
 
 const CaseStudy = () => {
   return (
-    <div>CaseStudy</div>
+    <div className="card-research-report">
+    {/* Header Section */}
+    <div className="card-research-header">
+      <p>CASE STUDY</p>
+      <h1>IIOT personalities that spark real connection</h1>
+      <h4>
+        IIOT: A Declaration of Autonomy - Advanced embedded IIOT solutions with
+        optimized performance and reliability.
+      </h4>
+      <div className="card-search-date">
+        <p>3-MINUTE READ</p>
+      </div>
+    </div>
+
+    <div className="case-study">
+      <img className="case-study-image" src={visual} alt="Visual representation" />
+    </div>
+
+    <div className="iiot-case-study">
+      <h1>Revolutionizing Industry with IIoT: The Future of Smart Manufacturing</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, rem. Nostrum molestiae fugiat quibusdam, esse voluptatum iure, quod alias deserunt maxime optio sit cupiditate placeat, tenetur enim quos adipisci dolores? Quia, magni maiores nobis odit ratione, perferendis autem nam mollitia ullam nulla officia possimus distinctio, accusantium quisquam id itaque praesentium explicabo qui nemo. Fugiat, iure esse perspiciatis quibusdam sapiente iste ad animi dignissimos! Laboriosam fuga ex rerum. Quos, fuga fugiat.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, rem. Nostrum molestiae fugiat quibusdam, esse voluptatum iure, quod alias deserunt maxime optio sit cupiditate placeat, tenetur enim quos adipisci dolores? Quia, magni maiores nobis odit ratione, perferendis autem nam mollitia ullam nulla officia possimus distinctio, accusantium quisquam id itaque praesentium explicabo qui nemo. Fugiat, iure esse perspiciatis quibusdam sapiente iste ad animi dignissimos! Laboriosam fuga ex rerum. Quos, fuga fugiat.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, rem. Nostrum molestiae fugiat quibusdam, esse voluptatum jure, quod alias deserunt maxime optio sit cupiditate placeat, tenetur enim quos adipisci dolores? Quia, magni maiores nobis odit ratione, perferendis Fugiat, iure esse perspiciatis quibusdam sapiente iste. Quos, fuga fugiat.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni animi eum amet corrupti deserunt nemo accusamus perferendis, nulla culpa aliquam facilis. Illum quo reiciendis numquam, officia distinctio exercitationem at maxime.</p>
+    </div>
+
+    <div className="iiot-case-study">
+      <h1>Revolutionizing Industry with IIoT: The Future of Smart Manufacturing</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, rem. Nostrum molestiae fugiat quibusdam, esse voluptatum iure, quod alias deserunt maxime optio sit cupiditate placeat, tenetur enim quos adipisci dolores? Quia, magni maiores nobis odit ratione, perferendis autem nam mollitia ullam nulla officia possimus distinctio, accusantium quisquam id itaque praesentium explicabo qui nemo. Fugiat, iure esse perspiciatis quibusdam sapiente iste ad animi dignissimos! Laboriosam fuga ex rerum. Quos, fuga fugiat.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, rem. Nostrum molestiae fugiat quibusdam, esse voluptatum iure, quod alias deserunt maxime optio sit cupiditate placeat, tenetur enim quos adipisci dolores? Quia, magni maiores nobis odit ratione, perferendis autem nam mollitia ullam nulla officia possimus distinctio, accusantium quisquam id itaque praesentium explicabo qui nemo. Fugiat, iure esse perspiciatis quibusdam sapiente iste ad animi dignissimos! Laboriosam fuga ex rerum. Quos, fuga fugiat.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, rem. Nostrum molestiae fugiat quibusdam, esse voluptatum iure, quod alias deserunt maxime optio sit cupiditate placeat, tenetur enim quos adipisci dolores? Quia, magni maiores nobis odit ratione, perferendis Fugiat, iure esse perspiciatis quibusdam sapiente iste. Quos, fuga fugiat.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni animi eum amet corrupti deserunt nemo accusamus perferendis, nulla culpa aliquam facilis. Illum quo reiciendis numquam, officia distinctio exercitationem at maxime.</p>
+    </div>
+
+    <div className="iiot-case-study">
+      <h1>Revolutionizing Industry with IIoT: The Future of Smart Manufacturing</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, rem. Nostrum molestiae fugiat quibusdam, esse voluptatum iure, quod alias deserunt maxime optio sit cupiditate placeat, tenetur enim quos adipisci dolores? Quia, magni maiores nobis odit ratione, perferendis autem nam mollitia ullam nulla officia possimus distinctio, accusantium quisquam id itaque praesentium explicabo qui nemo. Fugiat, iure esse perspiciatis quibusdam sapiente iste ad animi dignissimos! Laboriosam fuga ex rerum. Quos, fuga fugiat.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, rem. Nostrum molestiae fugiat quibusdam, esse voluptatum iure, quod alias deserunt maxime optio sit cupiditate placeat, tenetur enim quos adipisci dolores? Quia, magni maiores nobis odit ratione, perferendis Fugiat, iure esse perspiciatis quibusdam sapiente iste. Quos, fuga fugiat.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni animi eum amet corrupti deserunt nemo accusamus perferendis, nulla culpa aliquam facilis. Illum quo reiciendis numquam, officia distinctio exercitationem at maxime.</p>
+    </div>
+  </div>
   )
 }
 
---
+++ src/Components/Hero/HeroCommonCard/Perspective.jsx
@@ -0,0 +1,113 @@
+import React, { useRef } from 'react'
+import searchgif from "../HeroCardImages/searchgif.gif";
+import newsearch from "../HeroCardImages/newsearch.webm";
+
+const Perspective = () => {
+  const videoRef = useRef(null);
+  return (
+    <div className="card-research-report">
+      {/* Header Section */}
+      <div className="card-research-header">
+        <p>RESEARCH REPORT</p>
+        <h1>Technology Vision 2025</h1>
+        <h4>
+          PCB: A Declaration of Autonomy - Advanced embedded PCB solutions with
+          optimized performance and reliability.
+        </h4>
+        <div className="card-search-date">
+          <p>10-MINUTE READ</p>
+          &nbsp;
+          <p>FEBRUARY 7, 2025</p>
+        </div>
+      </div>
+
+      {/* Divider Line */}
+      <div className="card-search-more-line"></div>
+
+      {/* Brief Section */}
+      <div className="card-search-breef-heading">In brief</div>
+
+      {/* Brief Points */}
+      <div className="card-search-breef-points">
+        <div className="card-search-breef-content"></div>
+        <p className="card-search-breef-content-points">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum
+          impedit illo eos ipsa inventore perferendis voluptas quo repellat in
+          tempora?
+        </p>
+      </div>
+
+      <div className="card-search-breef-points">
+        <div className="card-search-breef-content"></div>
+        <p className="card-search-breef-content-points">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum
+          impedit illo eos ipsa inventore perferendis voluptas quo repellat in
+          tempora?
+        </p>
+      </div>
+
+      <div className="card-search-breef-points">
+        <div className="card-search-breef-content"></div>
+        <p className="card-search-breef-content-points">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum
+          impedit illo eos ipsa inventore perferendis voluptas quo repellat in
+          tempora?
+        </p>
+      </div>
+
+      {/* REAEARCH IMAGE CONTAINER */}
+      <div className="research-left-content-container">
+        <div className="research-header-line-container">
+          <section className="research-active-line"></section>
+          <h2>
+            Accelerating Autonomy with the Cognitive Digital Brain and
+            Technology
+          </h2>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat
+            laborum facere necessitatibus, similique sed doloremque praesentium
+            fugit ipsa, dolore porro recusandae, impedit quisquam perspiciatis
+            quasi error delectus non neque aliquid nesciunt qui magni odio
+            maiores? Rerum, amet! Laborum excepturi nisi, sed doloremque
+            delectus quibusdam dolorem aut in inventore amet ad facilis commodi
+            consequatur rerum nobis? Esse incidunt maiores aperiam amet, In
+            nihil eos asperiores. Consequuntur, vero!
+          </p>
+        </div>
+        <div className="research-right-image-container">
+          <div className="research-right-image-header-container">
+            <video
+              ref={videoRef}
+              className="search-video"
+              src={newsearch}
+              autoPlay
+              loop
+              muted
+              disablePictureInPicture
+              controlsList="nodownload nofullscreen noremoteplayback"
+              style={{ width: "600px", height: "350px" }}
+            />
+
+            {/* <img className='search-image' src={searchgif} alt='' style={{ width: '600px', height: '350px' }} /> */}
+
+            {/* <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, repellat.</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia possimus fugit deserunt facere, hic vitae ex, nesciunt quo delectus necessitatibus id quos, doloribus ullam mollitia officiis unde ad quisquam sit rem voluptatibus praesentium? Voluptas nostrum tempore laudantium libero deleniti officiis maiores commodi suscipit adipisci magnam recusandae obcaecati tempora perspiciatis provident, cumque qui corrupti blanditiis necessitatibus assumenda voluptate? Dolorem recusandae placeat id, quisquam aspernatur minus obcaecati officia cupiditate quo error explicabo?</p> */}
+          </div>
+        </div>
+      </div>
+
+                
+            {/* horizantal view */}
+      <div className='card-horizantal-view'>
+        <div className="card-horizantal-heading">
+            <h1>3 Emerging Trends of IIOt, autonomy and trust:</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum adipisci ab sapiente. Commodi eius cum cumque earum non suscipit. Ex blanditiis quod quas veniam itaque doloribus voluptatibus aperiam, enim maxime.</p>
+        </div>
+      </div>
+      <div className="three-emerging-lines-heading"> </div>
+      <footer ></footer>
+    </div>
+  );
+}
+
+export default Perspective
